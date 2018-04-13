a56068d002353d9961f8f203c6abb2ac917134a0
leecho <leecho571@vip.qq.com>
2018-04-13 22:50:46 +0800

    修改安全访问配置

--- cola-cloud-auth/src/main/java/com/honvay/cola/cloud/auth/configuration/ResourceServerConfiguration.java
+++ cola-cloud-auth/src/main/java/com/honvay/cola/cloud/auth/configuration/ResourceServerConfiguration.java
@@ -2,6 +2,7 @@ package com.honvay.cola.cloud.auth.configuration;
 
 import com.honvay.cola.cloud.auth.integration.IntegrationAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -19,9 +20,11 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.anonymous().disable()
+        http.anonymous().disable().csrf().disable()
             .authorizeRequests().antMatchers("/v2/api-docs", "/sms/token").permitAll()
             .and().authorizeRequests().anyRequest().fullyAuthenticated()
+            .and().logout().permitAll()
+            .and().formLogin().permitAll()
             .and().exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());
     }
 }
