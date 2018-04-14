b19bf7f9c97d47b33eeaa84a996578b2e24f64f2
leecho <leecho571@vip.qq.com>
2018-04-14 16:55:24 +0800

    优化安全访问配置

--- cola-cloud-framework/cola-cloud-framework-security/src/main/java/com/honvay/cola/cloud/framework/security/access/ResourceServiceSecurityAccessConfiguration.java
+++ cola-cloud-framework/cola-cloud-framework-security/src/main/java/com/honvay/cola/cloud/framework/security/access/ResourceServiceSecurityAccessConfiguration.java
@@ -3,8 +3,8 @@ package com.honvay.cola.cloud.framework.security.access;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfiguration;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 
@@ -12,7 +12,7 @@ import org.springframework.security.web.access.intercept.FilterSecurityIntercept
  * @author LIQIU
  * @date 2018-4-14
  **/
-@ConditionalOnBean(ResourceServerConfigurerAdapter.class)
+@ConditionalOnBean(ResourceServerConfiguration.class)
 public class ResourceServiceSecurityAccessConfiguration extends ResourceServerConfigurerAdapter {
 
     @Autowired
--- cola-cloud-framework/cola-cloud-framework-security/src/main/java/com/honvay/cola/cloud/framework/security/access/WebSecurityAccessConfiguration.java
+++ cola-cloud-framework/cola-cloud-framework-security/src/main/java/com/honvay/cola/cloud/framework/security/access/WebSecurityAccessConfiguration.java
@@ -2,18 +2,17 @@ package com.honvay.cola.cloud.framework.security.access;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfiguration;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 
 /**
  * @author LIQIU
  * @date 2018-4-14
  **/
-@ConditionalOnMissingBean(ResourceServerConfigurerAdapter.class)
+@ConditionalOnMissingBean(ResourceServerConfiguration.class)
 public class WebSecurityAccessConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
