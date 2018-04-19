9e94babf4972cd0ca602d1f87a20bc2e61534438
leecho <leecho571@vip.qq.com>
2018-04-19 19:27:25 +0800

    修改删除Token的接口

--- cola-cloud-auth/src/main/java/com/honvay/cola/cloud/auth/endpoint/TokenRevokeEndpoint.java
+++ cola-cloud-auth/src/main/java/com/honvay/cola/cloud/auth/endpoint/TokenRevokeEndpoint.java
@@ -7,8 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
+import org.springframework.security.oauth2.provider.endpoint.FrameworkEndpoint;
 import org.springframework.security.oauth2.provider.token.ConsumerTokenServices;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
@@ -18,7 +21,7 @@ import java.security.Principal;
  * @author LIQIU
  * @date 2018-3-26
  **/
-@RestController
+@FrameworkEndpoint
 @Api(tags = "登出接口")
 public class TokenRevokeEndpoint {
 
@@ -26,12 +29,10 @@ public class TokenRevokeEndpoint {
     @Qualifier("consumerTokenServices")
     private ConsumerTokenServices tokenServices;
 
-    @PostMapping("/oauth/logout")
+    @DeleteMapping("/oauth/token")
     @ApiOperation("退出登录")
-    public Result<String> deleteAccessToken(Principal principal){
-        OAuth2Authentication auth2Authentication = (OAuth2Authentication)principal;
-        OAuth2AuthenticationDetails details =  (OAuth2AuthenticationDetails)auth2Authentication.getDetails();
-        tokenServices.revokeToken(details.getTokenValue());
+    public Result<String> deleteAccessToken(@RequestParam("access_token") String accessToken){
+        tokenServices.revokeToken(accessToken);
         return Result.buildSuccess("");
     }
 
