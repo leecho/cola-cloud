80aa6eb07f2c6ea8b723062365e2f10a63993970
leecho <leecho571@vip.qq.com>
2018-04-20 14:02:36 +0800

    默认配置用户提取组件

--- cola-cloud-framework/cola-cloud-framework-oauth2/src/main/java/com/honvay/cola/cloud/framework/oauth2/token/UserPrincipalExtractorConfiguration.java
+++ cola-cloud-framework/cola-cloud-framework-oauth2/src/main/java/com/honvay/cola/cloud/framework/oauth2/token/UserPrincipalExtractorConfiguration.java
@@ -3,11 +3,13 @@ package com.honvay.cola.cloud.framework.oauth2.token;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerTokenServicesConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @author LIQIU
  * @date 2018-3-26
  **/
+@Configuration
 @AutoConfigureBefore(ResourceServerTokenServicesConfiguration.class)
 public class UserPrincipalExtractorConfiguration {
 
